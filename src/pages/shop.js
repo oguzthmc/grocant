cc44add946004e9f1a60ac61dba78056578dbaa9
oguzhan <oguzhan@lityum.com>
2024-07-12 16:22:45 +0300

    use grid, button and dropdown component to work on shop page

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,5 +1,134 @@
+import { Button, Col, Dropdown, Row } from 'antd';
+
+const products = [
+  {
+    key: '1',
+    image: require('../assets/images/img1.jpg'),
+    title:
+      'Fairy Clean & Fresh Washing Up Liquid Pomegranate &amp; Honeysuckle 520 ml',
+    price: '£2.00',
+  },
+  {
+    key: '2',
+    image: require('../assets/images/img2.jpg'),
+    title: 'Colgate Triple Action Toothpaste 100ml',
+    price: '£1.50',
+  },
+  {
+    key: '3',
+    image: require('../assets/images/img3.jpg'),
+    title: 'Breeze Toilet Tissue Soft White 24 Roll',
+    price: '£5.70',
+  },
+  {
+    key: '4',
+    image: require('../assets/images/img4.jpg'),
+    title: 'Comfort intense Fresh Sky Fabric Conditioner Liquid 60 Wash 900 ML',
+    price: '£4.50',
+  },
+  {
+    key: '5',
+    image: require('../assets/images/img5.jpg'),
+    title: 'Glens Vodka 70cl Extra smooth',
+    saleprice: '£20.00',
+    price: '£15.50',
+  },
+  {
+    key: '6',
+    image: require('../assets/images/img6.jpg'),
+    title: 'Pot Noodle Chicken & Mushroom Standard 90g',
+    saleprice: '£1.50',
+    price: '£0.90',
+  },
+  {
+    key: '7',
+    image: require('../assets/images/img7.jpg'),
+    title: 'Branston Baked Beans in a Rich and Tasty Tomato Sauce 4 x 410g',
+    saleprice: '£2.00',
+    price: '£1.50',
+  },
+  {
+    key: '8',
+    image: require('../assets/images/img8.jpg'),
+    title: 'Kelloggs Crunchy Nut Hazelnut & Chocolate Granola 380g',
+    saleprice: '£3.00',
+    price: '£2.50',
+  },
+];
+
+const items = [
+  {
+    key: '1',
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        Featured
+      </a>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        Price: Low to High
+      </a>
+    ),
+  },
+  {
+    key: '3',
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        Price: High to Low
+      </a>
+    ),
+  },
+];
+
 function AppShop() {
-  return <div>Shop</div>;
+  return (
+    <div className="block products shopPage">
+      <div className="container">
+        <div className="titleHolder">
+          <h2>Shop</h2>
+          <Dropdown
+            menu={{
+              items,
+            }}
+            placement="bottom"
+          >
+            <Button>Sort by</Button>
+          </Dropdown>
+        </div>
+        <Row gutter={24}>
+          {products.map((p) => {
+            return (
+              <Col lg={6} key={p.key}>
+                <div className="content">
+                  <div className="image">
+                    <img src={p.image} alt="productImage" />
+                  </div>
+                  <h3>{p.title}</h3>
+                  <div className="price">{p.price}</div>
+                  <Button type="primary">Add to Basket</Button>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+    </div>
+  );
 }
 
 export default AppShop;
