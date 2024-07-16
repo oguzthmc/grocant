e7814846570d46428fa16d09a7a13944c31ca542
oguzhan <oguzhan@lityum.com>
2024-07-16 16:15:16 +0300

    use collapse component to work on FAQ page

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,5 +1,56 @@
+import { Collapse } from 'antd';
+
+const text = `
+  Duo ea legimus legendos. Iusto aliquando quo in. Novum tritani ei pro, solum discere duo ad. Te mea dicant omnium, quem ornatus scribentur te his. Vis ei probatus efficiantur, virtute mentitum qui eu. Ex audiam audire nam, nec et iusto inermis. Tota ceteros definitiones ei vel, ad eos velit congue fabulas, adhuc urbanitas ex has. Modus labore cum ne, cu sea antiopam complectitur, persius necessitatibus at ius. Ut sed oblique intellegebat, nonumes voluptua constituto sed te. Eum idque falli fierent in. Utamur prodesset mei et.
+`;
+
+const items = [
+  {
+    key: '1',
+    label: 'Delivery and collection information',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '2',
+    label: 'What do I need to know about ordering online?',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '3',
+    label: 'Returns and refunds',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '4',
+    label: 'Managing my account',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '5',
+    label: 'Technical issues',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '6',
+    label: 'Terms and conditions',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '7',
+    label: 'Ratings and reviews',
+    children: <p>{text}</p>,
+  },
+];
+
 function AppFaq() {
-  return <div>Faq</div>;
+  return (
+    <div className="block faqPage">
+      <div className="container">
+        <h2>FAQ</h2>
+        <Collapse accordion items={items} defaultActiveKey={['1']} />
+      </div>
+    </div>
+  );
 }
 
 export default AppFaq;
